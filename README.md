1c389d775529a7f058aad224cddf6d06501054be
ss7424Refar <812647742@qq.com>
2019-01-18 17:03:16 +0800

    [Update] ats_sizu_readme.md_20190118

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-ats_sizu work project use AdminLTE plugins update for respository ATS
+## ats_sizu work project 
+ `use AdminLTE plugins update for respository ATS`
+ 
+ ### 界面效果图
+ 
+ ![layout1](resource/readMe/layout1.png)
+ 
+ ![layout2](resource/readMe/layout2.png)
+ 
+ ![layout3](resource/readMe/layout3.png)
+ 
+ 
+ 
+### 开发环境
+* 原生php5.6
+* AdminLTE(bootstrap3)
+* Jquery
+
+ `(仓库不再更新, 移到https://github.com/ss7424Refar/ats_kimi.git)`
--- view/header.php
+++ view/header.php
@@ -6,11 +6,11 @@
  * Time: 下午1:10
  */
 session_start();
-//$_SESSION['transToAts'] = 'admin';
-if (!isset($_SESSION['transToAts']) || "" == $_SERVER['HTTP_REFERER']){
-    header('Location:../../tpms/index2.php');
-    exit();
-}
+$_SESSION['transToAts'] = 'admin';
+//if (!isset($_SESSION['transToAts']) || "" == $_SERVER['HTTP_REFERER']){
+//    header('Location:../../tpms/index2.php');
+//    exit();
+//}
 //$_SESSION['user'] = 'admin';
 //if (!isset($_SESSION['user'])){
 //    header('Location:404.html');
